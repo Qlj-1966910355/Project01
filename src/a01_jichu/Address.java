42b52ddc743c0c0b3c3daadae0d0fb66017c1bc7
qlj <1966910355@qq.com>
2022-10-01 19:14:31 +0800

    2020-10-01 19:14 commit

--- src/a01_jichu/Address.java
+++ src/a01_jichu/Address.java
@@ -1,12 +1,12 @@
 package a01_jichu;// 地址类
 
-public class Address{
-	// 省份
-	String sf;
-	// 市
-	String sq;
-	// 县
-	String xj;
-	// 镇
-	String xz;
+public class Address {
+    // 省份
+    String sf;
+    // 市
+    String sq;
+    // 县
+    String xj;
+    // 镇
+    String xz;
 }
